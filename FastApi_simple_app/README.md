2335f143d46b51371b007b62ccc610b158d65ca1
Naveen <naveen.yerravarapu2000@gmail.com>
2025-12-28 10:36:57 +0530

    new changes to fastapi app, new api calls

--- FastApi_simple_app/README.md
+++ FastApi_simple_app/README.md
@@ -2,3 +2,12 @@
 
 docker build -t fastapi:v1 .
 docker run -p 8000:8000 fastapi:v1
+
+
+# to push to ECR (repo : dev_ecr_repo)
+
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 708735187026.dkr.ecr.us-east-1.amazonaws.com
+
+docker tag fastapi:v1 708735187026.dkr.ecr.us-east-1.amazonaws.com/dev_ecr_repo:v1
+
+docker push 708735187026.dkr.ecr.us-east-1.amazonaws.com/dev_ecr_repo:v1
--- FastApi_simple_app/main.py
+++ FastApi_simple_app/main.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, ValidationError
+import os
 
 app = FastAPI()
 
+version = os.environ.get("APP_VERSION", "v1.0.0")
+
 @app.get("/")
 def read_root():
-    return {"message": "hello from fastapi on eks", "version": "v1.0.0"}
+    return {"message": "hello from fastapi", "version": version}
 
 
 @app.get("/healthz")
@@ -14,3 +18,24 @@ def health_check():
 @app.get("/readyz")
 def readiness_check():
     return {"status": "ready"}
+
+@app.get("/tasks")
+def list_tasks():
+    items = ["cheese", "pepper", "bread"]
+    return {"items": items}
+
+class Task(BaseModel):
+    name: str
+    price: float
+
+@app.post("/tasks")
+def create_task(task: Task):
+    # FastAPI handles validation automatically before this line is reached
+    # try:
+    #     Task(**task)
+    # except ValidationError as e:
+    #     raise HTTPException(status_code=400, detail=str(e))
+    
+    if not task:
+        raise HTTPException(status_code=404, detail="Task is missing")
+    return {"task_created": task}
